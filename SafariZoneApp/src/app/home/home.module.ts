66aa7e965cdb85fd98ce57c1cb324b5333b97613
jstewart3800 <56611713+jstewart3800@users.noreply.github.com>
2020-03-05 15:36:04 -0800

    Start, Header, Search Button

---
+++ SafariZoneApp/src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import { HeaderComponent } from '../views/header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage,HeaderComponent] //Don't forget to declare the header on each module you need it
+})
+export class HomePageModule {}
