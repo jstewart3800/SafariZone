afb39c85baa999c96511f980fb56509fa3dcce24
Jon Stewart <56611713+jstewart3800@users.noreply.github.com>
2020-03-12 12:01:19 -0700

    Made log page

--- SafariZoneApp/src/app/app-routing.module.ts
+++ SafariZoneApp/src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
+    path: 'log',
+    loadChildren: () => import('./pages/log/log.module').then( m => m.LogPageModule)
+  },
+
 ];
 
 @NgModule({
