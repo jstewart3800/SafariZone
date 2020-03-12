86c6884ba721e38fbb14fd94a62155c03f46fa24
Jon Stewart <56611713+jstewart3800@users.noreply.github.com>
2020-03-12 16:09:01 -0700

    List Mockup, some style, Learn more about pages

--- SafariZoneApp/src/app/home/home.module.ts
+++ SafariZoneApp/src/app/home/home.module.ts
@@ -6,19 +6,25 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 import { HeaderComponent } from '../views/header/header.component';
+// import { LogPageModule } from '../pages/log/log.module';
+// import { LogPage } from '../pages/log/log.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
-  declarations: [HomePage,HeaderComponent] //Don't forget to declare the header on each module you need it
+     imports: [
+          CommonModule,
+          FormsModule,
+          IonicModule,
+          RouterModule.forChild([
+               {
+                    path: '',
+                    component: HomePage
+               },
+               {
+                    path: 'log',
+                    loadChildren: () => import('../pages/log/log.module').then(m => m.LogPageModule)
+               }
+          ])
+     ],
+     declarations: [HomePage, HeaderComponent] //Don't forget to declare the header on each module you need it
 })
-export class HomePageModule {}
+export class HomePageModule { }
