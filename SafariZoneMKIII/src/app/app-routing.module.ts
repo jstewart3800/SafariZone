55ee7cb83648823843e0c7d5770f9f679bd1d80c
Jon Stewart <56611713+jstewart3800@users.noreply.github.com>
2020-03-26 18:45:56 -0700

    Login Page, routing,  modal needs help

--- SafariZoneMKIII/src/app/app-routing.module.ts
+++ SafariZoneMKIII/src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+   {
+      path: '',
+      loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) //./tabs/tabs.module
+   },
+   {
+      path: 'tabs',
+      loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+   }
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+   imports: [
+      RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+   ],
+   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
